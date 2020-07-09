0425d86265c200da4b36f67660c38b7edf952d27
cansualtekin <cansu.altekin@santsg>
2020-07-09 22:05:53 +0300

    EF Core EnableRetryOnFailure feature enabled

---
+++ src/EntityFramework.Samples.ConnectionResiliency/Persistance/ConnectionResiliencyDbContext.cs
@@ -0,0 +1,17 @@
+﻿using EntityFramework.Samples.Shared.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EntityFramework.Samples.ConnectionResiliency.Persistance
+{
+    public class ConnectionResiliencyDbContext : DbContext
+    {
+            public ConnectionResiliencyDbContext(DbContextOptions<ConnectionResiliencyDbContext> options)
+           : base(options)
+            { }
+
+            public DbSet<User> Users { get; set; }
+    }
+}
---
+++ src/EntityFramework.Samples.ConnectionResiliency/Program.cs
@@ -0,0 +1,32 @@
+﻿using EntityFramework.Samples.ConnectionResiliency.Persistance;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace EntityFramework.Samples.ConnectionResiliency
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var services = new ServiceCollection();
+            services.AddDbContext<ConnectionResiliencyDbContext>(o =>
+            {
+                o.UseSqlServer("", options => {
+                    options.EnableRetryOnFailure();
+                });
+            });
+
+            // Service Provider servisleri çözebileceğimiz (resolve) bir containerdır.
+            var serviceProvider = services.BuildServiceProvider();
+            var context = serviceProvider.GetService<ConnectionResiliencyDbContext>();
+            context.Users.Add(new Shared.Entities.User
+            {
+                Name = "Cansu"
+            });
+
+            context.SaveChanges();
+            Console.ReadLine();
+        }
+    }
+}
